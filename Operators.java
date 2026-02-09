7bc8687511af3e248dac717470c7b0a291598bd0
S-Husan <241907483+S-Husan@users.noreply.github.com>
2026-02-09 12:02:57 +0500

    first commit

---
+++ Operators.java
@@ -0,0 +1,37 @@
+// New topic
+// Operators
+// -- is decriment
+//  ++ is enciment 
+// 
+
+
+
+
+
+public class Operators {
+    public static void main(String[] args)
+    {
+        int a = 9/2;   //4
+        System.out.println(a);
+        int b = 9;
+        System.out.println(b);
+        int c = 2;
+        System.out.println(" " + (a != c));
+        
+        // Shift operators
+        int r_shift = -2147483648;
+        int h = r_shift >> 1;
+        // int i = r_shift >> 1;
+        System.err.println(" h" + h); //0
+        // System.err.println(" i " + i); //0
+        int f = 4;
+        int g = f << 1;
+        System.out.println(" g" + g);
+
+
+    }
+}
+
+
+// we have 28 >>(zeros) abd 0100
+// 
