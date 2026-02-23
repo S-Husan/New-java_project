704dfda4d3b28d445dcc656c4fb48bf5901226f0
S-Husan <241907483+S-Husan@users.noreply.github.com>
2026-02-23 12:14:30 +0500

    examples and new topics

--- InputExample.java
+++ InputExample.java
@@ -11,31 +11,55 @@ public class InputExample {
         // String s ="let us extarct substring";
         // String j = s.substring(7);
         // System.out.println(j);
-        // String k =s.substring(7,14); 
+        // String k =s.substring(7,14);
         // System.out.println(k);
-        ///////////////////////////
-        String s = "Hello ";
-        String d = "Hello ";
+        //////////////////////////
+        // String s = "Hello ";
+        // String t = "Hello ";
 
         // checks content of object
-        int a = 100;
-        String p = s+a;
-        System.out.println(p);
-        if(s.equals(d)){
-            System.out.println("The contens are eqaul");
-        }
-        else{
-            System.out.println("The contents are not eqaul");
-        }
+        // int a = 100;
+        // String p = s+a;
+        // System.out.println(p);
+        // if(s.equals(d)){
+        // System.out.println("The contens are eqaul");
+        // }
+        // else{
+        // System.out.println("The contents are not eqaul");
+        // }
+        // // checks what type of object it referce
+        // if(s == d){
+        // System.out.println("They are eqal,their type");
+        // }
+        // else{
+        // System.out.println("They are not equal by type");
+        // }
+
+        // s = "Hello,world";
+        // System.out.println(s.equals(t));
+        // System.out.println(s == t);
+
+        // ................
+
+        // Empty string and null String
+        // String s = new String(""); Here, s referce to an object in which there are no characters.
     
-// checks what type of object it referce
-        if(s == d){
-            System.out.println("They are eqal,their type");
+        // String t = null; //Means there is nothing here.
+
+        // if(str.length() == 0) of if(str.equals(""))
+
+
+        // we can check the null string use the followig code
+        // if(str == null)
+
+        // example
+        String s = null;
+        if (s== null){
+            System.out.println("String is null,Operator is aborted");
         }
-        else{
-            System.out.println("They are not equal by type");
-        }
+        // if (s.length() == 0){
+
+        // }    
+
     }
 }
-    
-
