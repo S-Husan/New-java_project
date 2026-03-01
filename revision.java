8ad96d2984fdaf8e7a60308f47070562d407f2b9
S-Husan <241907483+S-Husan@users.noreply.github.com>
2026-03-01 14:53:39 +0500

    Here is revison of java

---
+++ revision.java
@@ -0,0 +1,21 @@
+public class revision {
+
+    public static void main(String[] args) {
+    int age = 50;
+
+    int year = 2025;
+    int quantity = 1;
+
+    double price = 19.99;
+    double gpa = 3.5;
+    double temperature = -12.5;
+
+    char grade = 'A' ;
+    char symbol = '!';
+    char currency = '$' ;
+
+    boolean isStudent = false;
+    boolean forSale = false;
+    boolean isOnline = true;        
+    }
+}
