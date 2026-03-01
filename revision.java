6acf48ae91553234efb8f9954123b8259421f802
S-Husan <241907483+S-Husan@users.noreply.github.com>
2026-03-01 16:11:50 +0500

    exampleee

--- revision.java
+++ revision.java
@@ -1,21 +1,43 @@
+import java.util.Scanner;
+
+
 public class revision {
 
+    // Primitive = simple value stored directly in memory (stack)
+    // Reference = memory address (stack) that points to the (heap)
+
     public static void main(String[] args) {
-    int age = 50;
 
-    int year = 2025;
-    int quantity = 1;
 
-    double price = 19.99;
-    double gpa = 3.5;
-    double temperature = -12.5;
 
-    char grade = 'A' ;
-    char symbol = '!';
-    char currency = '$' ;
 
-    boolean isStudent = false;
-    boolean forSale = false;
-    boolean isOnline = true;        
+        // JAVA START
+        // int age = 50;
+
+        // int year = 2025;
+        // int quantity = 1;
+
+        // double price = 19.99;
+        // double gpa = 3.5;
+        // double temperature = -12.5;
+
+        // char grade = 'A';
+        // char symbol = '!';
+        // char currency = '$';
+
+        // boolean isStudent = false;
+        // boolean forSale = false;
+        // boolean isOnline = true;
+        /////////////////////////////
+        
+        //User input in Java 
+        Scanner scanner = new Scanner(System.in);
+        // scanner.close();
+        System.out.println("Enter a number");
+        
+        scanner.close();
+
+         
+
     }
 }
