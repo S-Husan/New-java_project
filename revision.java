6fabcefb03f52396cd9f745ee8f2b0fd7f5ed9c8
S-Husan <241907483+S-Husan@users.noreply.github.com>
2026-03-11 00:46:16 +0500

    double and boolean with random function

--- revision.java
+++ revision.java
@@ -258,11 +258,25 @@ public static void main(String[] args) {
 // /////////////////////////////////////////////////////////////////////////////////////
 
     Random random = new Random();
-    int number;
-    number = random.nextInt(1,19);
-    System.out.println(number);
+    int number1;
+    int number2;
+    int number3;
 
 
+    number1 = random.nextInt(1,100);
+    number2 = random.nextInt(1,100);
+    number3 = random.nextInt(1,100);
+    System.out.println(number1);
+    System.out.println(number2);
+    System.out.println(number3);
+
+    double n; 
+    n = random.nextDouble();
+    System.out.println(n);
+
+    boolean name;
+    name = random.nextBoolean();
+    System.out.println(name);
 
 
 
