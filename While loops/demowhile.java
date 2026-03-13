e73e1d00360bc8984498640f0888eea943e56bb5
S-Husan <241907483+S-Husan@users.noreply.github.com>
2026-03-14 01:53:13 +0500

    While loop

---
+++ While loops/demowhile.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class demowhile {
+    public static void main(String[] args) {
+
+        // while loop = repeat some code forever
+        // while some condition remains true
+
+        Scanner scanner = new Scanner(System.in);
+        // String name = "";
+
+        // while(name.isEmpty()){
+        // System.out.print("Enter u name: ");
+        // name = scanner.nextLine();
+        // }
+
+        // System.out.println("Hello " + name);
+        ///////////////////////////////////////////////////////////
+
+        // String response = "";
+        // while(!response.equals("Q")){
+        // System.out.println("Print the Q to quit");
+        // response = scanner.next().toUpperCase();
+
+        // }
+
+        // System.out.println("You have quit the game");
+        ////////////////////////////////////////////////////////////
+
+        int age = 0;
+
+        System.out.print("Enter your age: ");
+        age = scanner.nextInt();
+
+        while (age < 0) {
+            System.out.println("Your age can't be nagative");
+            System.out.print("Enter your age: ");
+            age = scanner.nextInt();
+        }
+
+        System.out.println("your age is " + age);
+
+        ////////////////////////////////////////////////////////////
+        do  {
+            System.out.println("Your age can't be nagative");
+            System.out.print("Enter your age: ");
+            age = scanner.nextInt();
+        }while (age < 0);
+
+        System.out.println("your age is " + age);
+/////////////////////////////////////////////// 
+        scanner.close();
+    }
+}
