03f072b8133dc81477b838489b34271500714180
S-Husan <241907483+S-Husan@users.noreply.github.com>
2026-03-19 00:47:32 +0500

    prcitise

--- Lessons/2026.march/STUDENT.java
+++ Lessons/2026.march/STUDENT.java
@@ -3,26 +3,24 @@ import java.util.Scanner;
 class InnerSTUDENT {
     String name;
     int id;
-    double mark;
-    InnerSTUDENT(String name, int id,double mark){
-        this.name= name;
-        this.id= id;
-        this.mark = mark;
+    int[][] mark = { { 33, 45, 67 },
+                     { 22, 33, 64 },
+                     { 45, 84, 93 }
+};
+
+    InnerSTUDENT(String name, int id, double mark, double mark2, double mark3) {
+        this.name = name;
+        this.id = id;
+        this.mark = mark ;
 
     }
 }
 
 public class STUDENT {
     public static void main(String[] args) {
-        Student S1 = new Student("Husan", 17, 2);
-        Student S2 = new Student("Ali", 13, 3);
-        Student S3 = new Student("Kamron", 17, 2);
+        InnerSTUDENT S1 = new InnerSTUDENT("Husan", 17, 2, 3, 4);
+        InnerSTUDENT S2 = new InnerSTUDENT("Ali", 13, 3, 4, 6);
+        InnerSTUDENT S3 = new InnerSTUDENT("Kamron", 17, 2, 4, 3);
 
-
-
-
-
-
-        
     }
 }
