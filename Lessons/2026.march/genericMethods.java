a3e8f98f401249e8294db3c73fdc21ddbca59a56
S-Husan <241907483+S-Husan@users.noreply.github.com>
2026-04-06 11:19:32 +0500

    Prictise

--- Lessons/2026.march/genericMethods.java
+++ Lessons/2026.march/genericMethods.java
@@ -1,5 +1,23 @@
-public class genericMethods {
-    public static void main(String[] args){
-        
+class Utility<T> {
+static <T> void printArray(T[] arr){
+    for (T element :arr){
+        System.out.println(element + "");
+    }
+    System.out.println();
+}
+}
+
+class genericMethods {
+    public static void main(String[] args) {
+    
+        Integer[] intArr = {1,2,3,4};
+        String[] strArr = {"A","B","C"};
+        Double[] dblArr = {1.1,2.2,3.3};
+
+
+        Utility.printArray(intArr);
+        Utility.printArray(strArr);
+        Utility.printArray(dblArr);
+
     }
 }
