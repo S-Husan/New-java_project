c381aed37f24efe7bc5630ee3b8cffc414b7a918
S-Husan <241907483+S-Husan@users.noreply.github.com>
2026-04-06 11:38:09 +0500

    comments for generic methods

--- Lessons/2026.march/genericMethods.java
+++ Lessons/2026.march/genericMethods.java
@@ -1,23 +1,45 @@
+// Generic class (but note: class-level <T> is NOT used here)
 class Utility<T> {
-static <T> void printArray(T[] arr){
-    for (T element :arr){
-        System.out.println(element + "");
+
+    // Generic method (this <T> is separate from class <T>)
+    // It allows this method to work with ANY type (Integer, String, Double, etc.)
+    static <T> void printArray(T[] arr){
+
+        // Enhanced for-loop (for-each)
+        // Iterates through each element of the array
+        for (T element : arr){
+
+            // Print each element
+            // (element + "") converts element to String implicitly
+            System.out.println(element + "");
+        }
+
+        // Print empty line after each array output
+        System.out.println();
     }
-    System.out.println();
-}
 }
 
 class genericMethods {
     public static void main(String[] args) {
     
+        // Array of Integers
         Integer[] intArr = {1,2,3,4};
+
+        // Array of Strings
         String[] strArr = {"A","B","C"};
+
+        // Array of Doubles
         Double[] dblArr = {1.1,2.2,3.3};
 
+        // Calling the same generic method with different types
 
+        // Here T becomes Integer
         Utility.printArray(intArr);
-        Utility.printArray(strArr);
-        Utility.printArray(dblArr);
 
+        // Here T becomes String
+        Utility.printArray(strArr);
+
+        // Here T becomes Double
+        Utility.printArray(dblArr);
     }
 }
