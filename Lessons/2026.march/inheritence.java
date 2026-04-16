fdd43404d397c8428ec3a99081ca4304aa690146
S-Husan <241907483+S-Husan@users.noreply.github.com>
2026-04-16 10:18:27 +0500

    java inheretance

---
+++ Lessons/2026.march/inheritence.java
@@ -0,0 +1,75 @@
+class Animal{
+    String type;
+    Animal(String t){
+        type = t;
+        System.out.println("Animal constructor");
+    }
+    void eat(){
+        System.out.println("Animal is eating");
+    }
+    void sound(){
+        System.out.println("Animal sound");
+    }
+}
+
+class Dog extends Animal{
+    String name;
+    Dog(String t, String n){
+        System.out.println("Dog constructor");
+        name = n;
+        super(t); // execute Animal() constructor
+        //name = n;     
+    }
+    void bark(){
+        System.out.println("Dog barks");    
+    }
+
+    // @Override
+    void sound(){
+        System.out.println("Dog sound");
+    }
+}
+
+// class DogType extends Dog{
+//     DogType(){
+//         System.out.println("Dog type constructor");
+//     }
+// }
+
+public class inheritence     {
+    public static void main(String[] args) {
+        Dog d1 = new Dog("dog", "something");
+        
+        d1.eat(); // inherited
+        d1.bark(); // owned
+        d1.sound();
+        System.out.println(d1.name + " " + d1.type);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// java do not allow multiple class inheritence
+// not allowed: class A extends B, C
+// because of diamond problem
+// first parent constructor will be executed and then child class
+// multi-level inheritence
+// hierchery inheritence
